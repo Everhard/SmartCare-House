b2815eb4aeec834932fd0506fc6896aa3b91eaea
Andrew Dorokhov <andrew@dorohoff.net>
2016-05-11 02:38:19 +0300

    Add base HTML/CSS layout.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,74 +4,50 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
-
-AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    <link href="css/styles.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
-
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'My Company',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= $content ?>
-    </div>
-</div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
+<header>
+    <h1>SmartCare House</h1>
+    <section class="clock">
+      20:04
+    </section>
+</header>
+<nav class="zones">
+    <a href="#"><img src="img/icon-kitchen.png" alt="" /> Рабочий стол</a>
+    <a href="#"><img src="img/icon-kitchen.png" alt="" /> Кухня</a>
+    <a href="#"><img src="img/icon-kitchen.png" alt="" /> Прихожая</a>
+    <a href="#"><img src="img/icon-kitchen.png" alt="" /> Комната</a>
+    <a href="#"><img src="img/icon-kitchen.png" alt="" /> Чердак</a>
+</nav>
+<section class="main">
+    <h2>Рабочий стол</h2>
+    <?= $content ?>
+</section>
+<footer>
+    <nav>
+      <a href="#"><img src="img/timer.png" alt="" /></a>
+      <a href="#"><img src="img/timer.png" alt="" /></a>
+      <a class="settings" href="#"><img src="img/timer.png" alt="" /></a>
+    </nav>
 </footer>
-
+<script src="js/scripts.js"></script>
 <?php $this->endBody() ?>
 </body>
 </html>
